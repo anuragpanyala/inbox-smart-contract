33bba477371e5303a4b21d7c98779f02d9f485ec
anuragpanyala <anurag.panyala@gmail.com>
2018-07-24 21:48:30 -0700

    First commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+// const ganache = require('ganache-cli');
+
+const provider = new HDWalletProvider(
+    'farm angle slab end degree next pig visual high occur tourist spawn',
+    'https://rinkeby.infura.io/p7MT1Rnxz3M9sfi21lZn'
+);
+
+// const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const results = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: '0x' + bytecode, arguments: ['Hi there!']})
+    .send({from: accounts[0], gas: 1000000});
+
+    console.log('Contract deployed to', results.options.address);
+};
+deploy();
